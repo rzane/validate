38f698764037c2d10e4d03907d2abbd6643b398a
Ray Zane <raymondzane@gmail.com>
2020-05-17 20:56:55 -0400

    Rename step to validation

--- src/value.ts
+++ src/value.ts
@@ -1,39 +1,39 @@
 export type Cast<T, R> = (value: T) => R | Promise<R>;
 export type Predicate<T> = (value: T) => boolean | Promise<boolean>;
 
-export interface Chain<T> {
-  cast<R>(fn: Cast<T, R>): Chain<R>;
-  assert(fn: Predicate<T>, message?: string): Chain<T>;
-  refute(fn: Predicate<T>, message?: string): Chain<T>;
+export interface Validator<T> {
+  cast<R>(fn: Cast<T, R>): Validator<R>;
+  assert(fn: Predicate<T>, message?: string): Validator<T>;
+  refute(fn: Predicate<T>, message?: string): Validator<T>;
 }
 
-type Step = (value: any) => Promise<[any, any]>;
+type Validation = (value: any) => Promise<[any, any]>;
 
-const next = <T>(steps: Step[]): Chain<T> => ({
+const next = <T>(validations: Validation[]): Validator<T> => ({
   cast(fn) {
-    const step: Step = async (value) => {
+    const validation: Validation = async (value) => {
       return [await fn(value), undefined];
     };
 
-    return next([...steps, step]);
+    return next([...validations, validation]);
   },
   assert(fn, message = "is invalid") {
-    const step: Step = async (value) => {
+    const validation: Validation = async (value) => {
       const valid = await fn(value);
       const error = valid ? undefined : message;
       return [value, error];
     };
 
-    return next([...steps, step]);
+    return next([...validations, validation]);
   },
   refute(fn, message = "is invalid") {
-    const step: Step = async (value) => {
+    const validation: Validation = async (value) => {
       const invalid = await fn(value);
       const error = invalid ? message : undefined;
       return [value, error];
     };
 
-    return next([...steps, step]);
+    return next([...validations, validation]);
   },
 });
 
