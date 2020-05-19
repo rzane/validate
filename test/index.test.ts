4726e06c0a62d54ebdf38407930a9790a076c83a
Ray Zane <raymondzane@gmail.com>
2020-05-19 17:27:57 -0400

    Fix the exports test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,15 +1,5 @@
 import * as validate from "../src";
 
 test("exports", () => {
-  expect(Object.keys(validate).sort()).toEqual([
-    "assert",
-    "cast",
-    "isBlank",
-    "isNil",
-    "isNull",
-    "isString",
-    "isUndefined",
-    "refute",
-    "required"
-  ]);
+  expect(Object.keys(validate).sort()).toEqual(["validate"]);
 });
