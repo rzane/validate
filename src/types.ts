af54187d241b92207398fd160772735ccda256a0
Ray Zane <raymondzane@gmail.com>
2020-05-21 23:39:41 -0400

    Create types for assert and refute

--- src/types.ts
+++ src/types.ts
@@ -2,11 +2,9 @@ export type Forbid<T, X> = T extends X ? never : T;
 
 export type Transform<T, R> = (value: T) => R | Promise<R>;
 
+export type Assert<T, S extends T> = (value: T) => value is S;
+export type Refute<T, S> = (value: T | S) => value is S;
 export type Predicate<T> = (value: T) => boolean | Promise<boolean>;
-
-export type Assertion<T, S extends T> =
-  | ((value: T) => value is S)
-  | Predicate<T>;
 
 export interface Problem {
   message: string;
