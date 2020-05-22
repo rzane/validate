e925af34a964b1a39e6f947d2306690f2261d65d
Ray Zane <raymondzane@gmail.com>
2020-05-21 23:47:45 -0400

    Allow fewer type parameters for simple assert

--- src/operations.ts
+++ src/operations.ts
@@ -1,41 +1,10 @@
 import { Validator } from "./Validator";
-import { isUndefined, isNull, isNil } from "./predicates";
-import { Predicate, Transform, Assertion, Forbid } from "./types";
+import { isUndefined, isNull, isNil, not } from "./predicates";
+import { Transform, Forbid, Assert, Predicate } from "./types";
 
-/**
- * Transform the input value to a new value.
- */
-export const map = <T, R>(fn: Transform<T, R>): Validator<T, R> => {
-  return new Validator(async value => Validator.resolve(await fn(value)));
-};
+const MESSAGE = "This field is invalid.";
 
-/**
- * Produce an error if the value does not pass the test.
- */
-export const assert = <T, R extends T>(
-  fn: Assertion<T, R>,
-  message: string = "This field is invalid"
-): Validator<T, R> => {
-  return new Validator(async value => {
-    if (await fn(value)) {
-      return Validator.resolve(value as R);
-    } else {
-      return Validator.reject(message);
-    }
-  });
-};
-
-/**
- * Produce an error if the value passes the test.
- */
-export const refute = <T>(
-  fn: Predicate<T>,
-  message?: string
-): Validator<T, T> => {
-  return assert(async value => !(await fn(value)), message);
-};
-
-const createMaybe = <E>(test: Assertion<any, E>) => {
+const exclude = <E>(test: Assert<any, E>) => {
   return <T, R>(validator: Validator<Forbid<T, E>, R>): Validator<T, R | E> => {
     return new Validator(async input => {
       if (test(input)) {
@@ -50,14 +19,46 @@ const createMaybe = <E>(test: Assertion<any, E>) => {
 /**
  * Ignore undefined values when validating.
  */
-export const optional = createMaybe(isUndefined);
+export const optional = exclude(isUndefined);
 
 /**
  * Ignore null values when validating.
  */
-export const nullable = createMaybe(isNull);
+export const nullable = exclude(isNull);
 
 /**
  * Ignore null or undefined values when validating
  */
-export const maybe = createMaybe(isNil);
+export const maybe = exclude(isNil);
+
+/**
+ * Transform the input value to a new value.
+ */
+export const map = <T, R>(fn: Transform<T, R>): Validator<T, R> => {
+  return new Validator(async value => Validator.resolve(await fn(value)));
+};
+
+/**
+ * Produce an error if the value does not pass the test.
+ */
+export function assert<T, S extends T>(
+  fn: Assert<T, S>,
+  msg?: string
+): Validator<T, S>;
+export function assert<T>(fn: Predicate<T>, msg?: string): Validator<T, T>;
+export function assert(fn: Predicate<any>, msg = MESSAGE): Validator<any, any> {
+  return new Validator(async value => {
+    if (await fn(value)) {
+      return Validator.resolve(value);
+    } else {
+      return Validator.reject(msg);
+    }
+  });
+}
+
+/**
+ * Produce an error if the value passes the test.
+ */
+export const refute = <T>(fn: Predicate<T>, msg?: string): Validator<T, T> => {
+  return assert(not(fn), msg);
+};
