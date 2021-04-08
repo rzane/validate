3e158424c0b68ddd9ab92759a1cecffa79a949ee
Ray Zane <raymondzane@gmail.com>
2021-04-08 01:35:52 -0400

    Microbundle seems to produce a considerably smaller bundle size

--- src/operators/pass.ts
+++ src/operators/pass.ts
@@ -4,5 +4,5 @@ import { Validator } from "../Validator";
  * Skip validation for this field.
  */
 export function pass<T>(): Validator<T, T> {
-  return new Validator(async value => Validator.resolve(value));
+  return new Validator(value => Promise.resolve(Validator.resolve(value)));
 }
